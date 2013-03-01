52b4616300dc33e3da600fd6dfc0929bb6d6a29d
sdelano <stephen@opscode.com>
2013-03-01 14:47:40 -0800

    c-test: keep more detailed error information

--- correctness-test/scripts/validate.rb
+++ correctness-test/scripts/validate.rb
@@ -1,3 +1,21 @@
+# Failure data:
+# | orgname | node_name | error_code | error_message |
+FAILURES = []
+
+ROLE_CACHE = {}
+
+class Chef::RunList::RunListExpansionFromAPI
+  def fetch_role(name, included_by)
+    JSON.parse(ROLE_CACHE[name] ||= rest.get_rest("roles/#{name}").to_json)
+  rescue Net::HTTPServerException => e
+    if e.message == '404 "Not Found"'
+      role_not_found(name, included_by)
+    else
+      raise
+    end
+  end
+end
+
 nodes = api.get "nodes"
 
 results = nodes.keys.inject({}) do |res, node_name|
@@ -7,8 +25,14 @@ results = nodes.keys.inject({}) do |res, node_name|
                             api.post("environments/#{node_data.chef_environment}/cookbook_versions",
                                      :run_list => node_recipes)
                             "GOOD"
-                          rescue
-                            # todo: more sophisticated error handling
+                          rescue Net::HTTPServerException => e
+                            response = e.response
+                            code     = response.code
+                            message  = JSON.parse(e.response.body)['error']
+                            FAILURES << ["att_services",
+                                         node_name,
+                                         code,
+                                         message]
                             "ERROR"
                           end
   res[node_name] = resolved_dependencies
@@ -18,3 +42,5 @@ end
 puts "Total:   #{results.size}"
 puts "Success: #{results.select{|k,v| v == "GOOD"}.size}"
 puts "Failure: #{results.select{|k,v| v == "ERROR"}.size}"
+require 'pp'
+pp FAILURES
